30416222f667a290a1a8a74d256dae55ee58fad8
markekraus <github@markekraus.com>
2017-05-09 17:33:06 -0500

    DotSourced

---
+++ PesterClassIssue/Classes/TestClass.ps1
@@ -0,0 +1,12 @@
+Class TestClass {
+    static [string] $Satic = 'Static'
+    [string]$Property = 'Default'
+    [TestEnum]$Enum = [TestEnum]::Value1
+    static [string] StaticMethod () {
+        return 'Static Vlaue'
+    }
+    TestClass () {}
+    TestClass ([string]$Property) {
+        $This.Property = $Property
+    }
+}
